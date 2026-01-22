89fef8f38fa62dfba72c828dd5d1dc108e82b665
Diego Surita <suritadev@gmail.com>
2026-01-22 08:16:56 -0300

    change funciton name of spread operator solution in merge sorted arrays problem

--- src/array/merge-sorted-arrays/index.ts
+++ src/array/merge-sorted-arrays/index.ts
@@ -4,10 +4,13 @@
  * Run command: npx tsx index.ts
  */
 
-function mergeSortedArrays(arr1: number[], arr2: number[]) {
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+function spreadOperator(arr1: number[], arr2: number[]) {
     return [...arr1, ...arr2].sort((item1, item2) => item1 - item2)
 }
 
 console.log(
-    mergeSortedArrays([0,3,4,31], [4,6,30])
+    'reverse using spread operator',
+    spreadOperator([0,3,4,31], [4,6,30])
 )
